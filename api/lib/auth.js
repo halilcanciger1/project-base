5f80037029a29b8637071e23f6f96381e3aa6385
halilcanciger1 <149325624+halilcanciger1@users.noreply.github.com>
2025-01-20 17:57:55 +0300

    initial commit

--- api/lib/auth.js
+++ api/lib/auth.js
@@ -1,29 +1,32 @@
 const passport = require("passport");
-const { ExtractJwt, Strategy} = require("passport-jwt");
-const UserRoles = require("../db/models/UserRoles");
-const RolePrivileges = require("../db/models/RolesPrivileges");
+const { ExtractJwt, Strategy } = require("passport-jwt");
+const Users = require("../db/Users");
+const UserRoles = require("../db/UserRoles");
+const RolePrivileges = require("../db/RolesPrivileges");
 const Response = require("./Response");
-const { HTTP_CODES } = require("../config/Enum");
+const { HTTP_CODES } = require("../config/enum");
+
 const config = require("../config/config1");
+
 const privs = require("../config/role_privileges");
 const CustomError = require("./Error");
-const Users = require("../db/Users");
 
-module.exports = function() {
+module.exports = function () {
     let strategy = new Strategy({
         secretOrKey: config.JWT.SECRET,
-        jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken()
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
     }, async (payload, done) => {
         try {
+
             let user = await Users.findOne({ _id: payload.id });
 
-            if(user){
+            if (user) {
 
-                let userRoles = await UserRoles.find({ user_id : payload.id });
+                let userRoles = await UserRoles.find({ user_id: payload.id });
 
-                let rolePrivileges = await RolePrivileges.find({ role_id: { $in: userRoles.map(ur => ur.role_id) }});
+                let rolePrivileges = await RolePrivileges.find({ role_id: { $in: userRoles.map(ur => ur.role_id) } });
 
-                let privileges = rolePrivileges.map(rp => privs.privileges.find(x => x.key == rp.permisson));
+                let privileges = rolePrivileges.map(rp => privs.privileges.find(x => x.key == rp.permission))
 
                 done(null, {
                     id: user._id,
@@ -35,22 +38,24 @@ module.exports = function() {
                     exp: parseInt(Date.now() / 1000) + config.JWT.EXPIRE_TIME
                 });
 
+            } else {
+                done(new Error("User not found"), null);
             }
-            else{
-                done(new Error("User not found"), null)
-            }
+
         } catch (err) {
             done(err, null);
         }
+
     });
+
     passport.use(strategy);
 
     return {
-        initialize: function() {
+        initialize: function () {
             return passport.initialize();
         },
-        authenticate: function() {
-            return passport.authenticate("jwt", { session: false});
+        authenticate: function () {
+            return passport.authenticate("jwt", { session: false })
         }
     }
 }
--- api/routes/users.js
+++ api/routes/users.js
@@ -7,7 +7,9 @@ const bcrypt = require("bcrypt");
 const validator = require('validator');
 const Roles = require("../db/Roles");
 const UserRoles = require("../db/UserRoles");
-
+const auth = require("../lib/auth");
+const config = require('../config/config1');
+const jwt = require('jwt-simple');
 
 var router = express.Router();
 
@@ -207,5 +209,39 @@ router.post('/register', async (req, res) => {
   }
 });
 
+
+router.post("/auth", async (req, res) => {
+  try {
+
+    let { email, password } = req.body;
+
+    Users.validateFieldsBeforeAuth(email, password);
+
+    let user = await Users.findOne({ email });
+
+    if(!user) throw new CustomError(Enum.HTTP_CODES.UNAUTHORIZED, "Validation Error", "Email or Password wrong");
+
+    if(!user.validPassword(password)) throw new CustomError(Enum.HTTP_CODES.UNAUTHORIZED, "Validation Error", "Email or Password wrong");
+
+    let payload = {
+      id: user._id,
+      exp: parseInt(Date.now() / 1000) + config.JWT.EXPIRE_TIME
+    }
+
+    let token = jwt.encode(payload, config.JWT.SECRET);
+
+    let userData = {
+      _id: user._id,
+      first_name: user.first_name,
+      last_name: user.last_name
+    }
+
+    res.json(Response.successResponse({ token, user: userData }));
+
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+})
 
 module.exports = router;
